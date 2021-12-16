1b2445a659790eb5dd0a8696b35c43fa5bc7468a
Lukas Bombach <lukas.bombach@googlemail.com>
2021-12-16 16:46:47 +0100

    using a child compiler

--- compile-to-string-loader.js
+++ compile-to-string-loader.js
@@ -1,13 +1,40 @@
-/**  * @type {import("webpack").LoaderDefinition} */
-module.exports = async function compiletoStringLoader(content, map, meta) {
-  const callback = this.async();
+module.exports = function compileToStringLoader() {
+  const COMPILER_NAME = "vanillaExtractJsCompiler";
+  const outputOptions = { filename: "[id].js" };
+  const loaderCallback = this.async();
 
-  const result = await this.importModule(this.resourcePath);
+  const childCompiler = this._compilation.createChildCompiler(COMPILER_NAME, outputOptions, [
+    new this._compiler.webpack.EntryPlugin(this._compiler.context, this.resourcePath),
+  ]);
 
-  // here i am just returning the original content, but what I really want to do is return
-  // the compiled contents of the requested file as a string, so kind of like
-  //
-  // callback(null, `module.exports = ${JSON.stringify(result)}`);
+  childCompiler.runAsChild(async (error, entries, childCompilation) => {
+    const { warnings, errors } = childCompilation?.getStats().toJson() ?? {};
+    const source = childCompilation?.assets[`${entries[0].id}.js`]?.source();
 
-  callback(null, content, map, meta);
+    if (warnings.length) {
+      warnings.forEach(warning => console.warn(warning));
+    }
+
+    if (error) {
+      loaderCallback(error);
+      return;
+    }
+
+    if (errors.length) {
+      loaderCallback(new Error(errors[0].message));
+      return;
+    }
+
+    if (!source) {
+      loaderCallback(new Error("Failed to get source in vanilla extract loader"));
+      return;
+    }
+
+    if (error) {
+      loaderCallback(error);
+      return;
+    }
+
+    loaderCallback(null, `module.exports = ${JSON.stringify(source)};`);
+  });
 };
